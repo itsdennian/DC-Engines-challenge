54ab481e8918abe6cc2f5df468ee6754fd65118e
Denny P. Sam <blackstorm.denny@gmail.com>
2017-05-29 11:57:39 +0530

    Angle of sector function added
    Random value returned by angle of point function for testing

--- pie_pie_ka_jawab.py
+++ pie_pie_ka_jawab.py
@@ -1,6 +1,5 @@
 import math
 
-
 def isInside(x,y):
 	dist = math.pow((x-50),2) + math.pow((y-50),2)
 	if math.sqrt(dist) <= 50:
@@ -8,8 +7,12 @@ def isInside(x,y):
 	else:
 		return False
 
+def angle_sector(perc):
+	return float(float(perc)/100*360)
 
 
+def angle_point(x_coord,y_coord):
+	return 10
 
 def borw(perc, x_coord, y_coord):
 	if (isInside(x_coord,y_coord) and angle_point(x_coord,y_coord) < angle_sector(perc)):
