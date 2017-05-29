0657751e300ab877029ce77a72910f81b0d4bb40
Denny P. Sam <blackstorm.denny@gmail.com>
2017-05-29 11:25:43 +0530

    Figured out conditions
    Working on the math formulas required to calculate the required params

--- pie_pie_ka_jawab.py
+++ pie_pie_ka_jawab.py
@@ -1,3 +1,26 @@
+import math
+
+
+def isInside(x,y):
+
+
+
+
+
+def borw(perc, x_coord, y_coord):
+	if (isInside(x_coord,y_coord) and angle_point(x_coord,y_coord) < angle_sector(perc)):
+		return 'black'
+	else:
+		return 'white'
+
+
+
+
+
+
 t = int(input())
 for q in range(t):
 	perc, x_coord, y_coord = map(int, input().split(' '))
+	print('Case #'+str(q+1)+':', end =' ')
+	print(borw(perc,x_coord,y_coord))
+
